83d8cc5cd117b351abaae6545dd9d93d6c49b5c8
Suzuki <63243808+Suzuringo@users.noreply.github.com>
2020-04-07 14:50:06 +0900

    Update index.php
    不要な記述を削除

--- index.php
+++ index.php
@@ -10,16 +10,16 @@
 <body>
     <div class="slider">
         <ul class="slider-ul">
-            <li id="i1"><img src="img/1.webp" alt=""></li>
-            <li id="i2"><img src="img/2.jpg" alt=""></li>
-            <li id="i3"><img src="img/3.webp" alt=""></li>
-            <li id="i4"><img src="img/4.jpg" alt=""></li>
-            <li id="i5"><img src="img/5.jpg" alt=""></li>
-            <li id="i6"><img src="img/6.webp" alt=""></li>
-            <li id="i7"><img src="img/7.jpg" alt=""></li>
-            <li id="i8"><img src="img/8.jfif" alt=""></li>
-            <li id="i9"><img src="img/9.jfif" alt=""></li>
-            <li id="i10"><img src="img/10.jfif" alt=""></li>
+            <li><img src="img/1.webp" alt=""></li>
+            <li><img src="img/2.jpg" alt=""></li>
+            <li><img src="img/3.webp" alt=""></li>
+            <li><img src="img/4.jpg" alt=""></li>
+            <li><img src="img/5.jpg" alt=""></li>
+            <li><img src="img/6.webp" alt=""></li>
+            <li><img src="img/7.jpg" alt=""></li>
+            <li><img src="img/8.jfif" alt=""></li>
+            <li><img src="img/9.jfif" alt=""></li>
+            <li><img src="img/10.jfif" alt=""></li>
         </ul>
     </div>
 </body>
